599b254ad5813892a61ccf917a141c65cd9076f9
MrTinker64 <avi.micah@gmail.com>
2023-06-26 16:23:54 -0700

    + all the module constants, kP is arbitrary

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,6 +5,12 @@ import edu.wpi.first.math.util.Units;
 public final class Constants {
   public static class ModuleConstants {
     public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
+    public static final double kDriveMotorGearRatio = 6.12;
+    public static final double kTurningMotorGearRatio = 150 / 7;
+    public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
+    public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
+    public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
+    public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
     public static final double kPTurning = 0.5; // *
   }
   public static class OperatorConstants {}
