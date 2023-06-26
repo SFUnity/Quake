b3f6c0b38bb4f4dac32d49d05594d011f1fa68f8
MrTinker64 <avi.micah@gmail.com>
2023-06-26 16:19:02 -0700

    Finished setting up the SwerveModule class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,11 @@
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
+
 public final class Constants {
+  public static class ModuleConstants {
+    public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
+    public static final double kPTurning = 0.5; // *
+  }
   public static class OperatorConstants {}
 }
--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -1,5 +1,65 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.AnalogInput;
+import frc.robot.Constants.ModuleConstants;
+
 public class SwerveModule {
     
+    private final CANSparkMax m_driveMotor;
+    private final CANSparkMax m_turningMotor;
+
+    private final RelativeEncoder m_driveEncoder;
+    private final RelativeEncoder m_turningEncoder;
+
+    private final AnalogInput m_AbsoluteEncoder;
+    private final double kAbsoluteEncoderOffsetRad;
+    private final boolean kAbsoluteEncoderReversed;
+
+    private final PIDController turningPidController;
+    
+    public SwerveModule(int kDriveMotorId, int kTurningMotorId, boolean driveMotorReversed, boolean turningMotorReversed,
+            int absoluteEncoderId, double absoluteEncoderOffset, boolean absoluteEncoderReversed) {
+        
+        m_driveMotor = new CANSparkMax(kDriveMotorId, MotorType.kBrushless);
+        m_turningMotor = new CANSparkMax(kTurningMotorId, MotorType.kBrushless);
+
+        m_driveMotor.setInverted(driveMotorReversed);
+        m_turningMotor.setInverted(turningMotorReversed);
+
+        m_driveEncoder = m_driveMotor.getEncoder();
+        m_turningEncoder = m_turningMotor.getEncoder();
+
+        kAbsoluteEncoderOffsetRad = absoluteEncoderOffset;
+        kAbsoluteEncoderReversed = absoluteEncoderReversed;
+        m_AbsoluteEncoder = new AnalogInput(absoluteEncoderId);
+
+        turningPidController = new PIDController(ModuleConstants.kPTurning, 0, 0); // Consider adding the kI & kD
+        turningPidController.enableContinuousInput(-Math.PI, Math.PI);
+    }
+
+    public double getDrivePosition() {
+        return m_driveEncoder.getPosition();
+    }
+
+    public double getTurningPosition() {
+        return m_turningEncoder.getPosition();
+    }
+
+    public double getDriveVelocity() {
+        return m_driveEncoder.getVelocity();
+    }
+
+    public double getTurningVelocity() {
+        return m_turningEncoder.getVelocity();
+    }
+    
+    public void resetEncoders() {
+        m_driveEncoder.setPosition(0);
+        m_turningEncoder.setPosition(kAbsoluteEncoderOffsetRad);
+    }
 }
