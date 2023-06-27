03cdb5067ae9288bb9c87357b5a55d026e69c0f6
MrTinker64 <avi.micah@gmail.com>
2023-06-27 10:41:44 -0700

    Added the constants for the SwerveModule

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,7 +3,7 @@ package frc.robot;
 import edu.wpi.first.math.util.Units;
 
 public final class Constants {
-  public static class ModuleConstants {
+  public static final class ModuleConstants {
     public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
     public static final double kDriveMotorGearRatio = 6.12;
     public static final double kTurningMotorGearRatio = 150 / 7;
@@ -13,5 +13,16 @@ public final class Constants {
     public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
     public static final double kPTurning = 0.5; // *
   }
-  public static class OperatorConstants {}
+
+  public static final class DriveConstants {
+    // Distance between right and left wheels
+    public static final double kTrackWidth = Units.inchesToMeters(22.75);
+    // Distance between front and back wheels
+    public static final double kWheelBase = Units.inchesToMeters(22.75);
+
+    public static final double kPhysicalMaxSpeedMetersPerSecond = 4.25;
+    public static final double kPhysicalMaxAngularSpeedRadiansPerSecond = 2 * 2 * Math.PI;
+  }
+
+  public static final class OperatorConstants {}
 }
