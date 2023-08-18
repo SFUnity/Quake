4cb7a9517ed42fe7fa964ca3d07526d16639e262
MrTinker64 <avi.micah@gmail.com>
2023-08-18 11:53:04 -0400

    Imported HAL

--- src/test/java/SwerveModuleTest.java
+++ src/test/java/SwerveModuleTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import edu.wpi.first.hal.HAL;
 import frc.robot.subsystems.SwerveModule;
 
 public class SwerveModuleTest {
