64736c2f596cd496024d951483a9eb14865c036a
MrTinker64 <avi.micah@gmail.com>
2023-08-18 11:50:26 -0400

    Wrote the basics for the SwerveModuleTest

---
+++ src/test/java/SwerveModuleTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import frc.robot.subsystems.SwerveModule;
+
+public class SwerveModuleTest {
+    // scientific notation for acceptable deviation range
+    static final double DELTA = 1e-2;
+    SwerveModule m_SwerveModule;
+
+    @BeforeEach
+    void setup() {
+        assert HAL.initialize(500, 0);
+        m_SwerveModule = new SwerveModule();
+    }
+
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
+    @AfterEach
+    void shutdown() throws Exception {
+        m_intake.close(); // destroy our intake object
+    }
+}
