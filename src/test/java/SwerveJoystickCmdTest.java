8b5f1d9a900744104fae8e4bcd58987c270e5416
MrTinker64 <avi.micah@gmail.com>
2023-08-21 13:36:56 -0700

    Setup class and wrote testEnd() method, it passes!

---
+++ src/test/java/SwerveJoystickCmdTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import frc.robot.commands.SwerveJoystickCmd;
+import frc.robot.subsystems.SwerveSubsystem;
+import static org.mockito.Mockito.*;
+
+public class SwerveJoystickCmdTest {
+
+    SwerveSubsystem subsystem;
+    SwerveJoystickCmd command;
+    
+    @BeforeEach
+    public void setup() {
+        // Arrange
+        subsystem = mock(SwerveSubsystem.class);
+    }
+
+    @Test
+    public void testEnd() {
+        // Arrange
+        command = new SwerveJoystickCmd(subsystem, null, null, null, null);
+        // Act
+        command.end(false);
+        // Assert
+        verify(subsystem).stopModules();
+    }
+}
