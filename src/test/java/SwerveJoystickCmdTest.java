24b1977b6b65de66531263b23977f6dc74c1b1ac
MrTinker64 <avi.micah@gmail.com>
2023-08-21 14:33:55 -0700

    Still testing workflow

--- src/test/java/SwerveJoystickCmdTest.java
+++ src/test/java/SwerveJoystickCmdTest.java
@@ -23,6 +23,6 @@ public class SwerveJoystickCmdTest {
         // Act
         command.end(false);
         // Assert
-        verify(subsystem).getRotation2d();
+        verify(subsystem).zeroHeading();
     }
 }
