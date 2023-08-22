e261e305eb7940caf0143b94f8d3565fa650832c
MrTinker64 <avi.micah@gmail.com>
2023-08-22 15:35:16 -0700

    Nested the tests that need the setup method

--- src/test/java/SwerveModuleTest.java
+++ src/test/java/SwerveModuleTest.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -23,41 +24,45 @@ public class SwerveModuleTest {
         assertEquals(offset * (reversed ? -1.0 : 1.0), subsystem.getAbsoluteEncoderRad());
     }
 
-    @BeforeEach
-    void setup() {
-        // Arrange
-        subsystem = new SwerveModule(0, 1, 
-            false, false, 2, 
-            0, false);
-    }
+    @Nested
+    public class TestsWithBasicSwerveModule {
+        @BeforeEach
+        void setup() {
+            // Arrange
+            subsystem = new SwerveModule(0, 1, 
+                false, false, 2, 
+                0, false);
+        }
 
-    @Test
-    public void testResetEncoders() {
-        // Act and Assert
-        subsystem.resetEncoders();
-        assertEquals(0, subsystem.getDrivePosition());
-        assertEquals(subsystem.getAbsoluteEncoderRad(), subsystem.getTurningPosition());
-    }
+        @Test
+        public void testResetEncoders() {
+            // Act and Assert
+            subsystem.resetEncoders();
+            assertEquals(0, subsystem.getDrivePosition());
+            assertEquals(subsystem.getAbsoluteEncoderRad(), subsystem.getTurningPosition());
+        }
 
-    @Test
-    public void testGetState() {
-        // Act and Assert
-        assertEquals(new SwerveModuleState(subsystem.getDriveVelocity(), new Rotation2d(subsystem.getTurningPosition())), subsystem.getState());
-    }
+        @Test
+        public void testGetState() {
+            // Act and Assert
+            assertEquals(new SwerveModuleState(subsystem.getDriveVelocity(), new Rotation2d(subsystem.getTurningPosition())), subsystem.getState());
+        }
 
-    @Test
-    public void testGetDrivePosition() {
-        // Act and Assert
-        System.out.println("Drive position = " + subsystem.getDrivePosition());
-        assertTrue(subsystem.getDrivePosition() >= 0);
-    }
+        @Test
+        public void testGetDrivePosition() {
+            // Act and Assert
+            System.out.println("Drive position = " + subsystem.getDrivePosition());
+            assertTrue(subsystem.getDrivePosition() >= 0);
+        }
 
-    @Test
-    public void testGetTurningPosition() {
-        // Act and Assert
-        System.out.println("Drive position = " + subsystem.getTurningPosition());
-        assertTrue(subsystem.getTurningPosition() >= 0);
+        @Test
+        public void testGetTurningPosition() {
+            // Act and Assert
+            System.out.println("Drive position = " + subsystem.getTurningPosition());
+            assertTrue(subsystem.getTurningPosition() >= 0);
+        }
     }
+    
 
     @AfterEach
     void shutdown() throws Exception {
