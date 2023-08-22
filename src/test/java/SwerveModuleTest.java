580ce62654dbe8f249095183770be5a2aa298fec
MrTinker64 <avi.micah@gmail.com>
2023-08-22 16:12:54 -0700

    Removed AbsEnc tests bc they weren't useful

--- src/test/java/SwerveModuleTest.java
+++ src/test/java/SwerveModuleTest.java
@@ -3,7 +3,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -13,54 +12,40 @@ import frc.robot.subsystems.SwerveModule;
 public class SwerveModuleTest {
 
     SwerveModule subsystem;
-
-    private void testAbsoluteEncoderTemplate(double offset, boolean reversed) {
+    @BeforeEach
+    void setup() {
         // Arrange
         subsystem = new SwerveModule(0, 1, 
             false, false, 2, 
-            offset, reversed);
-        // Act and Assert
-        System.out.println(subsystem.getAbsoluteEncoderRad());
-        assertEquals(offset * (reversed ? -1.0 : 1.0), subsystem.getAbsoluteEncoderRad());
+            0, false);
     }
 
-    @Nested
-    public class TestsWithBasicSwerveModule {
-        @BeforeEach
-        void setup() {
-            // Arrange
-            subsystem = new SwerveModule(0, 1, 
-                false, false, 2, 
-                0, false);
-        }
+    @Test
+    public void testResetEncoders() {
+        // Act and Assert
+        subsystem.resetEncoders();
+        assertEquals(0, subsystem.getDrivePosition());
+        assertEquals(subsystem.getAbsoluteEncoderRad(), subsystem.getTurningPosition());
+    }
 
-        @Test
-        public void testResetEncoders() {
-            // Act and Assert
-            subsystem.resetEncoders();
-            assertEquals(0, subsystem.getDrivePosition());
-            assertEquals(subsystem.getAbsoluteEncoderRad(), subsystem.getTurningPosition());
-        }
+    @Test
+    public void testGetState() {
+        // Act and Assert
+        assertEquals(new SwerveModuleState(subsystem.getDriveVelocity(), new Rotation2d(subsystem.getTurningPosition())), subsystem.getState());
+    }
 
-        @Test
-        public void testGetState() {
-            // Act and Assert
-            assertEquals(new SwerveModuleState(subsystem.getDriveVelocity(), new Rotation2d(subsystem.getTurningPosition())), subsystem.getState());
-        }
+    @Test
+    public void testGetDrivePosition() {
+        // Act and Assert
+        System.out.println("Drive position = " + subsystem.getDrivePosition());
+        assertTrue(subsystem.getDrivePosition() >= 0);
+    }
 
-        @Test
-        public void testGetDrivePosition() {
-            // Act and Assert
-            System.out.println("Drive position = " + subsystem.getDrivePosition());
-            assertTrue(subsystem.getDrivePosition() >= 0);
-        }
-
-        @Test
-        public void testGetTurningPosition() {
-            // Act and Assert
-            System.out.println("Drive position = " + subsystem.getTurningPosition());
-            assertTrue(subsystem.getTurningPosition() >= 0);
-        }
+    @Test
+    public void testGetTurningPosition() {
+        // Act and Assert
+        System.out.println("Drive position = " + subsystem.getTurningPosition());
+        assertTrue(subsystem.getTurningPosition() >= 0);
     }
     
 
