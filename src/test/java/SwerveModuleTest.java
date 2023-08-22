555f88f130412acf27b2502022a17979ca5402b0
MrTinker64 <avi.micah@gmail.com>
2023-08-22 07:54:31 -0700

    Added a test for  getDrivePosition

--- src/test/java/SwerveModuleTest.java
+++ src/test/java/SwerveModuleTest.java
@@ -1,25 +1,27 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import edu.wpi.first.hal.HAL;
 import frc.robot.subsystems.SwerveModule;
 
 public class SwerveModuleTest {
-    static final double DELTA = 1e-2; // acceptable deviation range in sci. notation
-    SwerveModule m_SwerveModule;
+
+    SwerveModule subsystem;
 
     @BeforeEach
     void setup() {
-        assert HAL.initialize(500, 0);
-        m_SwerveModule = new SwerveModule(0, 0, false, false, 0, 0, false);
     }
 
-    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
-    @AfterEach
-    void shutdown() throws Exception {
-        m_SwerveModule.close(); // destroy our intake object
+    @Test
+    public void testGetDrivePosition() {
+        // Arrange
+        subsystem = new SwerveModule(0, 1, 
+            false, false, 2, 
+            0, false);
+        // Act and Assert
+        assertTrue(-180 <= subsystem.getDrivePosition() && subsystem.getDrivePosition() <= 180);
+        System.out.println("Drive position = " + subsystem.getDrivePosition());
     }
 }
