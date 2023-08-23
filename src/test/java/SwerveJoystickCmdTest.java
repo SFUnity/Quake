de0561d042f9d03821a13815b2f41112a02cf230
MrTinker64 <avi.micah@gmail.com>
2023-08-23 09:32:48 -0700

    Consolidated mocks and set up for  precise tests

--- src/test/java/SwerveJoystickCmdTest.java
+++ src/test/java/SwerveJoystickCmdTest.java
@@ -1,8 +1,11 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.SwerveJoystickCmd;
 import frc.robot.subsystems.SwerveSubsystem;
 
@@ -10,27 +13,57 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.Mockito.*;
 
+import java.util.function.Supplier;
+
 public class SwerveJoystickCmdTest {
 
-    SwerveSubsystem subsystem;
-    SwerveJoystickCmd command;
+    @Mock SwerveSubsystem subsystem;
     CommandXboxController controller;
+    SwerveJoystickCmd command;
+
+    @Mock private Supplier<Double> xSpdFunction, ySpdFunction, turningSpdFunction;
+    @Mock private Trigger fieldOrientedFunction;
 
     @BeforeEach
     public void setup() {
         // Arrange
-        subsystem = mock(SwerveSubsystem.class);
+        MockitoAnnotations.openMocks(this);
+
         controller = new CommandXboxController(0);
+        
+        when(xSpdFunction.get()).thenReturn(0.5);
+        when(ySpdFunction.get()).thenReturn(0.5);
+        when(turningSpdFunction.get()).thenReturn(0.5);
+        when(fieldOrientedFunction.getAsBoolean()).thenReturn(false);
+
         command = spy(new SwerveJoystickCmd(
                 subsystem,
-                () -> -controller.getLeftY(),
-                () -> controller.getLeftX(),
-                () -> controller.getRightX(),
-                controller.y()));
+                xSpdFunction, 
+                ySpdFunction, 
+                turningSpdFunction,
+                fieldOrientedFunction));
     }
 
     @Test
-    public void testExecute() {
+    public void testExecuteApproximate() {
+        // Act
+        command.execute();
+        // Assert
+        verify(command).applyDeadBandXSpeed(anyDouble());
+        verify(command).applyDeadBandYSpeed(anyDouble());
+        verify(command).applyDeadBandTurningSpeed(anyDouble());
+
+        verify(command).smoothXSpeed(anyDouble());
+        verify(command).smoothYSpeed(anyDouble());
+        verify(command).smoothTurningSpeed(anyDouble());
+
+        verify(command).speedsToChassisSpeeds(anyDouble(), anyDouble(), anyDouble());
+        
+        verify(subsystem).setModuleStates(any(SwerveModuleState[].class));
+    }
+
+    @Test
+    public void testExecuteExact() {
         // Act
         command.execute();
         // Assert
