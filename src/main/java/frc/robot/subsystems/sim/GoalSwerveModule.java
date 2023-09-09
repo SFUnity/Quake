2f5d179395ca29c691d0ea7fcd89e2f05d59256e
MrTinker64 <avi.micah@gmail.com>
2023-09-09 15:34:46 -0700

    added new class for simulation purposes

---
+++ src/main/java/frc/robot/subsystems/sim/GoalSwerveModule.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems.sim;
+
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+/** Ideal swerve module, useful for debugging */
+public class GoalSwerveModule {
+
+  private SwerveModuleState state = new SwerveModuleState();
+  private double distance;
+
+  public SwerveModuleState getState() {
+    return state;
+  }
+
+  public SwerveModulePosition getPosition() {
+    return new SwerveModulePosition(distance, state.angle);
+  }
+
+  public void setDesiredState(SwerveModuleState _desiredState) {
+    state = SwerveModuleState.optimize(_desiredState, state.angle);
+    distance += state.speedMetersPerSecond * 0.02;
+  }
+
+  public SwerveModuleState getDesiredState() {
+    return state;
+  }
+
+  public void resetEncoders() {}
+
+}
