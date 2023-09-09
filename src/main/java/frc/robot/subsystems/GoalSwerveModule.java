8f2d5038775c81729380c8659a5551a7cc08dce6
MrTinker64 <avi.micah@gmail.com>
2023-09-09 15:36:29 -0700

    Moved some stuff

--- src/main/java/frc/robot/subsystems/GoalSwerveModule.java
+++ src/main/java/frc/robot/subsystems/GoalSwerveModule.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems.sim;
+package frc.robot.subsystems;
 
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
