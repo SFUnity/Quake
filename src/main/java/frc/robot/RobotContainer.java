575f24f1f709f31175eb4941ca8fe0cfe4feb271
MrTinker64 <avi.micah@gmail.com>
2023-12-18 18:04:29 -0800

    added a simple straight auto w/ shuffleboard chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,8 @@ package frc.robot;
 
 import com.pathplanner.lib.PathConstraints;
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -19,11 +21,14 @@ public class RobotContainer {
     private final CommandXboxController m_driverController = new CommandXboxController(
                     OperatorConstants.kDriverControllerPort);
 
+    // Auto Commands
     private final CommandFactory commandFactory;
 
     private final Command m_complexAuto;
 
-    private final Command m_simpleAuto = new StraightAutoCmd(m_swerveSubsystem);
+    private final Command m_straightAuto = new StraightAutoCmd(m_swerveSubsystem);
+
+    SendableChooser<Command> m_chooser = new SendableChooser<>();
 
     public RobotContainer() {
         commandFactory = new CommandFactory(m_swerveSubsystem);
@@ -42,17 +47,25 @@ public class RobotContainer {
                 m_driverController.y()));
 
         configureBindings();
+
+        // Add commands to the autonomous command chooser
+        m_chooser.setDefaultOption("Complex Auto", m_complexAuto);
+
+        m_chooser.addOption("Straight Auto", m_straightAuto);
+
+        // Put the chooser on the dashboard
+        Shuffleboard.getTab("Auto Options").add(m_chooser);
     }
 
   private void configureBindings() {
     new Trigger(m_driverController.a()).onTrue(new InstantCommand(() -> m_swerveSubsystem.zeroHeading()));
   }
-
-  public Command getAutonomousCommand() {
-      return m_complexAuto;
-  }
   
   public SwerveSubsystem getSwerveSubsystem() {
       return m_swerveSubsystem;
   }
+
+  public Command getAutonomousCommand() {
+      return m_chooser.getSelected();
+  }  
 }
