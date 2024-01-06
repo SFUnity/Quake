1746225ada768c1a183508a1d4b5f114d07ed156
William Coffer <wzcoffer@gmail.com>
2024-01-06 13:58:52 -0800

    logs don't create errors when simulating

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.SwerveSubsystem;
+import edu.wpi.first.wpilibj.RobotBase;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
@@ -18,7 +19,11 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     // Logging stuff
-    DataLogManager.start("/home/lvuser");
+    if (RobotBase.isReal()) {
+      DataLogManager.start("/home/lvuser");
+    } else {
+      DataLogManager.start("logs");
+    }
     DataLog log = DataLogManager.getLog();
     DriverStation.startDataLog(log);
 
