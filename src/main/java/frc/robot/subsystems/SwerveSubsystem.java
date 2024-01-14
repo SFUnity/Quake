4c6ddba2b209ba1cc50a9dca40232abd8344cc60
Lholden <65085083+imagine45@users.noreply.github.com>
2024-01-14 15:30:12 -0800

    Set up auto for swerve

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -23,10 +23,14 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.FieldObject2d;
 import frc.robot.Constants.DriveConstants;
 import lib.SwerveModule;
+import com.pathplanner.lib.auto.*;
+import com.pathplanner.lib.util.*;
+
 
 public class SwerveSubsystem extends SubsystemBase implements AutoCloseable {
     private final SwerveModule m_frontLeft = SwerveModule.create(
@@ -135,6 +139,32 @@ public class SwerveSubsystem extends SubsystemBase implements AutoCloseable {
             } catch (Exception e) {
             }
         }).start();
+
+        AutoBuilder.configureHolonomic(
+            this::getPose, // Robot pose supplier
+            this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+            this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+            new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+                    new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                    new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+                    4.5, // Max module speed, in m/s
+                    0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+                    new ReplanningConfig() // Default path replanning config. See the API for the options here
+            ),
+            () -> {
+                // Boolean supplier that controls when the path will be mirrored for the red alliance
+                // This will flip the path being followed to the red side of the field.
+                // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+                var alliance = DriverStation.getAlliance();
+                if (alliance.isPresent()) {
+                    return alliance.get() == DriverStation.Alliance.Red;
+                }
+                return false;
+            },
+            this // Reference to this subsystem to set requirements
+        );
     }
 
     public Pose2d getPose() {
@@ -271,4 +301,6 @@ public class SwerveSubsystem extends SubsystemBase implements AutoCloseable {
         m_backRight.close();
         m_gyro.close();
     }
+
+    
 }
