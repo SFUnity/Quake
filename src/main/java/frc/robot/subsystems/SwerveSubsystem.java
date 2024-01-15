3b021f5bd24fee70ae64093d0fe8ef993bded0bc
Lholden <65085083+imagine45@users.noreply.github.com>
2024-01-14 16:26:39 -0800

    replanning

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -151,7 +151,7 @@ public class SwerveSubsystem extends SubsystemBase implements AutoCloseable {
                     new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
                     4.5, // Max module speed, in m/s
                     0.4, // Drive base radius in meters. Distance from robot center to furthest module.
-                    new ReplanningConfig() // Default path replanning config. See the API for the options here
+                    new ReplanningConfig(false, false) // Default path replanning config. See the API for the options here
             ),
             () -> {
                 // Boolean supplier that controls when the path will be mirrored for the red alliance
