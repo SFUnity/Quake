eb0d2323e97784b546912f2eceba790c15a9c701
Avi Katzman <avi.micah@gmail.com>
2024-01-14 17:34:34 -0800

    changed turning speed

--- src/main/java/frc/robot/commands/SwerveJoystickCmd.java
+++ src/main/java/frc/robot/commands/SwerveJoystickCmd.java
@@ -42,9 +42,10 @@ public class SwerveJoystickCmd extends Command {
         ySpeed = this.applyDeadBand(ySpeed);
         turningSpeed = this.applyDeadBand(turningSpeed);
         
+        // Modified speeds
         xSpeed *= 0.3;
         ySpeed *= 0.3;
-        turningSpeed *= -0.4;
+        turningSpeed *= -0.6;
         
         ChassisSpeeds chassisSpeeds = speedsToChassisSpeeds(xSpeed, ySpeed, turningSpeed, fieldOrientedFunction);
 
