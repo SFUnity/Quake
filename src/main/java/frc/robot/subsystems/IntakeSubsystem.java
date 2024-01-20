6f85c99b59be4f4e9f1808ede63288552b96e530
William Coffer <wzcoffer@gmail.com>
2024-01-20 15:34:25 -0800

    added rev robotics dependencies for distance sensor

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,9 +1,6 @@
 package main.java.frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.CANSparkMax.IdleMode;
-import frc.robot.Constants.ArmConstants;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
