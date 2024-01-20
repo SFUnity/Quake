3d2a7297cd3d350b1dc371158ddc76091944acb7
William Coffer <wzcoffer@gmail.com>
2024-01-20 15:06:50 -0800

    created intake file with libs

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,14 @@
+package main.java.frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import frc.robot.Constants.ArmConstants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class IntakeSubsystem extends SubsystemBase{
+    public IntakeSubsystem() {
+        
+    }
+}
