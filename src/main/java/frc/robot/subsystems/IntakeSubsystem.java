3d4ad1f898e6b5114b5783cfce5d70cef28d7824
S C <bramblestar1.0@icloud.com>
2024-01-21 15:25:13 -0800

    FInished up intake

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
@@ -15,7 +16,21 @@ public class IntakeSubsystem extends SubsystemBase{
     
     private final CANcoder m_encoder = new CANcoder(IntakeConstants.kIntakeAngleMotorEncoderPort);
 
+    private final PIDController m_IntakePID = new PIDController(0.05, 0, 0); //mess around with this later
+
     public IntakeSubsystem() {
         
+    }
+
+    public void moveIntake(double speed) {
+        m_IntakeAngleMotor.set(speed);
+    }
+
+    public void runIntake(double speed) {
+        m_IntakeRollersMotor.set(speed);
+    }
+
+    public void setIntakeToAngle(double angle) {
+        moveIntake(m_IntakePID.calculate(m_encoder.getAbsolutePosition().getValueAsDouble(), angle));
     }
 }
