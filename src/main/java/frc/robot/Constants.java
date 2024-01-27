afbef10efed67c8247b5dfc0ec5612e92ac87274
MINAJECHUNG <mchung27@urbanschool.org>
2024-01-27 14:38:50 -0800

    got periotic to work we think

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -103,6 +103,8 @@ public final class Constants {
     public static final int kShooterFlywheelMotor = 12;
 
     public static final int kShooterAngleMotorEncoderPort = 5;
+    public static final Double kShooterAngleMotorEncoderOffset = 0.0;
+    public static final Double kShooterMotorMaxSpeed = 10.0;
     
   }
 }
