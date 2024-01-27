980165175bf39a866da6a4e934228c263217092c
S C <bramblestar1.0@icloud.com>
2024-01-27 13:50:53 -0800

    changed a onstant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -95,7 +95,7 @@ public final class Constants {
     public static final int kShooterAngleMotor = 11;
     public static final int kShooterFlywheelMotor = 12;
 
-    public static final int kIntakeAngleMotorEncoderPort = 5;
+    public static final int kShooterAngleMotorEncoderPort = 5;
     
   }
 }
