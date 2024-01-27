1cce3b971be515b6131fa4c1b7de479b42ac8f08
William Coffer <wzcoffer@gmail.com>
2024-01-27 14:05:54 -0800

    periodic override for moving arm

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,11 +2,11 @@ package frc.robot.subsystems;
 import frc.robot.Constants.IntakeConstants;
 
 import com.ctre.phoenix6.hardware.CANcoder;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-//import com.revrobotics.Rev2mDistanceSensor.Port;
-//import com.revrobotics.Rev2mDistanceSensor;
+
+import com.revrobotics.*;
+import com.revrobotics.Rev2mDistanceSensor.Port;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -21,11 +21,23 @@ public class Intake extends SubsystemBase{
 
     private final PIDController m_IntakePID = new PIDController(0.05, 0, 0); //mess around with this later
 
-    //private final Rev2mDistanceSensor noteSensor;
+    private final Rev2mDistanceSensor distOnboard, distMXP;
+    private Double angle = 0.0;
+    private Boolean intakeMoving = false;
 
     public Intake() {
         // add port
-        //noteSensor = new Rev2mDistanceSensor();
+        distOnboard = new Rev2mDistanceSensor(Port.kOnboard);
+        distMXP = new Rev2mDistanceSensor(Port.kMXP);
+        distOnboard.setAutomaticMode(true);
+    }
+
+    @Override
+    public void periodic() {
+        super.periodic();
+        if (intakeMoving) {
+            moveIntake(m_IntakePID.calculate(m_encoder.getAbsolutePosition().getValueAsDouble(), angle));
+        }
     }
 
     public void moveIntake(double speed) {
@@ -37,6 +49,7 @@ public class Intake extends SubsystemBase{
     }
 
     public void setIntakeToAngle(double angle) {
-        moveIntake(m_IntakePID.calculate(m_encoder.getAbsolutePosition().getValueAsDouble(), angle));
+        intakeMoving = true;
+        this.angle = angle;
     }
 }
