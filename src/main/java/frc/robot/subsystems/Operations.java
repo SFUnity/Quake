871029a0e18f48b5028fdb632c96b3eedd4a3986
MrTinker64 <avi.micah@gmail.com>
2024-01-26 17:48:11 -0800

    rainbow!

--- src/main/java/frc/robot/subsystems/Operations.java
+++ src/main/java/frc/robot/subsystems/Operations.java
@@ -7,11 +7,10 @@ public class Operations {
     // PWM port 9
     // Must be a PWM header, not MXP or DIO
     private final AddressableLED m_led = new AddressableLED(9);
-
     // Reuse buffer
     // Default to a length of 60, start empty output
     private final AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(60);
-
+    private int m_rainbowFirstPixelHue;
 
     public Operations() {
         // Length is expensive to set, so only set it once, then just update data
@@ -22,11 +21,26 @@ public class Operations {
         m_led.start();
     }
 
+    public void rainbow() {
+        // For every pixel
+        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+          // Calculate the hue - hue is easier for rainbows because the color
+          // shape is a circle so only one value needs to precess
+          final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+          // Set the value
+          m_ledBuffer.setHSV(i, hue, 255, 128);
+        }
+        // Increase by to make the rainbow "move"
+        m_rainbowFirstPixelHue += 3;
+        // Check bounds
+        m_rainbowFirstPixelHue %= 180;
+      }
+
     public void setGreen() {
         setRGB(0, 255, 0);
     }
     
-    private void setRGB(int r, int g, int b) {
+    public void setRGB(int r, int g, int b) {
         for (var i = 0; i < m_ledBuffer.getLength(); i++) {
             // Sets the specified LED to the RGB values for red
             m_ledBuffer.setRGB(i, r, g, b);
