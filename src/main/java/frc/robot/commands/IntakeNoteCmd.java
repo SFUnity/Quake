0124a93949ac09c7b6b56b9e13258a4e1a8f0f3b
William Coffer <wzcoffer@gmail.com>
2024-02-03 14:13:15 -0800

    skeleton for intake note comand

--- src/main/java/frc/robot/commands/IntakeNoteCmd.java
+++ src/main/java/frc/robot/commands/IntakeNoteCmd.java
@@ -6,7 +6,38 @@ import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Operations;
 
 public class IntakeNoteCmd extends Command{
+
+    private final Intake intake;
+    private final Shooter shooter;
+    private final Operations operations;
+
     public IntakeNoteCmd(Intake intake, Shooter shooter, Operations operations) {
-        
+        this.intake = intake;
+        this.shooter = shooter;
+        this.operations = operations;
+    }
+
+    @Override
+    public void initialize() {
+        // TODO Auto-generated method stub
+        super.initialize();
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        super.execute();
+    }
+
+    @Override
+    public boolean isFinished() {
+        // TODO Auto-generated method stub
+        return super.isFinished();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        // TODO Auto-generated method stub
+        super.end(interrupted);
     }
 }
