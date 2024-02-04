f7f5fbb3af266dcb7fe6dcfd6689f004014703fa
William Coffer <wzcoffer@gmail.com>
2024-02-03 16:34:45 -0800

    removed unecessary import of supliers

--- src/main/java/frc/robot/commands/IntakeControllerCmd.java
+++ src/main/java/frc/robot/commands/IntakeControllerCmd.java
@@ -1,7 +1,5 @@
 package frc.robot.commands;
 
-import java.util.function.Supplier;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.LEDConstants;
