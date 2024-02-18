d8d9579222b1fc882f1222e95ba8a09a32793bff
MrTinker64 <avi.micah@gmail.com>
2024-02-17 17:30:55 -0800

    swapped out for krakens

--- src/main/java/frc/robot/subsystems/modules/RealSwerveModule.java
+++ src/main/java/frc/robot/subsystems/modules/RealSwerveModule.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.modules;
 
 import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
@@ -16,10 +17,9 @@ import lib.SwerveModule;
 
 public class RealSwerveModule implements AutoCloseable, SwerveModule {
     
-    private final CANSparkMax m_driveMotor;
+    private final TalonFX m_driveMotor;
     private final CANSparkMax m_turningMotor;
 
-    private final RelativeEncoder m_driveEncoder;
     private final RelativeEncoder m_turningEncoder;
 
     private final CANcoder m_absoluteEncoder;
@@ -32,13 +32,12 @@ public class RealSwerveModule implements AutoCloseable, SwerveModule {
     public RealSwerveModule(int kDriveMotorId, int kTurningMotorId, boolean driveMotorReversed, boolean turningMotorReversed,
             int absoluteEncoderId, boolean absoluteEncoderReversed) {
         
-        m_driveMotor = new CANSparkMax(kDriveMotorId, MotorType.kBrushless);
+        m_driveMotor = new TalonFX(kDriveMotorId);
         m_turningMotor = new CANSparkMax(kTurningMotorId, MotorType.kBrushless);
 
         m_driveMotor.setInverted(driveMotorReversed);
         m_turningMotor.setInverted(turningMotorReversed);
 
-        m_driveEncoder = m_driveMotor.getEncoder();
         m_turningEncoder = m_turningMotor.getEncoder();
 
         kAbsoluteEncoderReversed = absoluteEncoderReversed;
@@ -49,26 +48,6 @@ public class RealSwerveModule implements AutoCloseable, SwerveModule {
 
         resetEncoders(); // Resets encoders every time the robot boots up
     }
-
-    // // ! For testing purposes only
-    // public RealSwerveModule(CANSparkMax driveMotor, CANSparkMax turningMotor, 
-    //         AnalogInput absoluteEncoder, double absoluteEncoderOffset, boolean absoluteEncoderReversed) {
-       
-    //     m_driveMotor = driveMotor;
-    //     m_turningMotor = turningMotor;
-
-    //     m_driveEncoder = m_driveMotor.getEncoder();
-    //     m_turningEncoder = m_turningMotor.getEncoder();
-        
-    //     kAbsoluteEncoderOffset = absoluteEncoderOffset;
-    //     kAbsoluteEncoderReversed = absoluteEncoderReversed;
-    //     m_absoluteEncoder = absoluteEncoder;
-
-    //     turningPidController = new PIDController(ModuleConstants.kPTurning, 0, 0); // Consider adding the kI & kD
-    //     turningPidController.enableContinuousInput(-Math.PI, Math.PI);
-
-    //     resetEncoders();
-    // }
 
     @Override
     public SwerveModuleState getState() {
@@ -93,7 +72,7 @@ public class RealSwerveModule implements AutoCloseable, SwerveModule {
 
     @Override
     public void resetEncoders() {
-        m_driveEncoder.setPosition(0);
+        m_driveMotor.setPosition(0);
         m_turningEncoder.setPosition(getAbsoluteEncoderRotations());
     }
 
@@ -114,11 +93,11 @@ public class RealSwerveModule implements AutoCloseable, SwerveModule {
     }
 
     public double getDrivePosition() {
-        return m_driveEncoder.getPosition();
+        return m_driveMotor.getPosition().getValueAsDouble();
     }
 
     public double getDriveVelocity() {
-        return m_driveEncoder.getVelocity();
+        return m_driveMotor.getVelocity().getValueAsDouble();
     }
 
     @Override
