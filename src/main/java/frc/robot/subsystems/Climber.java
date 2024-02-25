e0eac1c5f9bd932732a00145e4e601c4684142ef
William Coffer <wzcoffer@gmail.com>
2024-02-25 15:47:59 -0800

    removed unecessary dependency

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -6,7 +6,6 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
-import frc.robot.commands.ClimberCmd;
 
 
 public class Climber extends SubsystemBase{
