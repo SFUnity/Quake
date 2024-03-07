70b5fcff5bacb7c88cfc5dfbe186acd041334e1d
MrTinker64 <avi.micah@gmail.com>
2024-03-06 21:01:55 -0800

    dpad done

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -73,10 +73,10 @@ public class RobotContainer {
                 () -> -m_driverController.getLeftX(),
                 () -> m_driverController.getRightX(),
                 m_driverController.leftBumper(),
-                m_driverController.povUp(),
                 m_driverController.povDown(),
-                m_driverController.povRight(),
+                m_driverController.povUp(),
                 m_driverController.povLeft(),
+                m_driverController.povRight(),
                 true));
 
         m_shooter.setDefaultCommand(new ShooterCmd(
