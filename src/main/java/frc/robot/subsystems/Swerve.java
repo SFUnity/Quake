11faa5ade4ff8066d74126def1ea3c6a5009ba9b
Avi Katzman <avi.micah@gmail.com>
2024-03-08 12:01:13 -0800

    auto field orientation working

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -23,6 +23,7 @@ import edu.wpi.first.networktables.DoubleArrayPublisher;
 import edu.wpi.first.networktables.DoubleArrayTopic;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -163,11 +164,10 @@ public class Swerve extends SubsystemBase implements AutoCloseable {
                 // This will flip the path being followed to the red side of the field.
                 // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
 
-                // var alliance = DriverStation.getAlliance();
-                /* if (alliance.isPresent()) {
+                var alliance = DriverStation.getAlliance();
+                if (alliance.isPresent()) {
                     return alliance.get() == DriverStation.Alliance.Red;
                 }
-                */
                 return false;
             },
             this // Reference to this subsystem to set requirements
