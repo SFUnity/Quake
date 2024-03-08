a6b56d20b7d1de78620a6ee7703c55e8ca7b083b
Avi Katzman <avi.micah@gmail.com>
2024-03-07 16:59:55 -0800

    made this work better

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -251,18 +251,13 @@ public class Swerve extends SubsystemBase implements AutoCloseable {
         m_backRight.setDesiredState(desiredStates[3]);
         
         desiredModuleStates[0] = desiredStates[0].angle.getDegrees();
-        desiredModuleStates[1] = desiredStates[1].speedMetersPerSecond;
-        desiredModuleStates[2] = desiredStates[2].angle.getDegrees();
-        desiredModuleStates[3] = desiredStates[3].speedMetersPerSecond;
-        desiredModuleStates[4] = desiredStates[4].angle.getDegrees();
-        desiredModuleStates[5] = desiredStates[5].speedMetersPerSecond;
-        desiredModuleStates[6] = desiredStates[6].angle.getDegrees();
-        desiredModuleStates[7] = desiredStates[7].speedMetersPerSecond;
-
-        for (int i = 0; i < modules.size(); i++) {
-            desiredModuleStates[i] = desiredStates[i].angle.getDegrees();
-            desiredModuleStates[i + 1] = desiredStates[i].speedMetersPerSecond;
-        }
+        desiredModuleStates[1] = desiredStates[0].speedMetersPerSecond;
+        desiredModuleStates[2] = desiredStates[1].angle.getDegrees();
+        desiredModuleStates[3] = desiredStates[1].speedMetersPerSecond;
+        desiredModuleStates[4] = desiredStates[2].angle.getDegrees();
+        desiredModuleStates[5] = desiredStates[2].speedMetersPerSecond;
+        desiredModuleStates[6] = desiredStates[3].angle.getDegrees();
+        desiredModuleStates[7] = desiredStates[3].speedMetersPerSecond;
         
         m_desiredStatesPublisher.set(desiredModuleStates);
     }
