96490a35a2633528a4df297024fd62171d8e4f95
Avi Katzman <avi.micah@gmail.com>
2024-03-16 14:47:00 -0700

    small merge error fix

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -29,7 +29,6 @@ import frc.robot.subsystems.*;
 
 
 public class RobotContainer {
-    private final LEDs m_LEDs = new LEDs();
     private final Swerve m_swerve = new Swerve();
     public final LimelightSubsystem m_limelight = LimelightSubsystem.getInstance();
     private final LEDs m_LEDs = new LEDs();
